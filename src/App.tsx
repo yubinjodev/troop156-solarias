79fa05290034900cf2fc1434db7aa4e8ceab0a01
Yubin <yubinjodev@gmail.com>
2023-06-30 00:04:09 +0900

    create folder structure and create theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from "@emotion/react";
+import { themeOptions } from "./styles/theme";
+
 function App() {
   return (
+    <ThemeProvider theme={themeOptions}>
     <div className="App">
       asdf
     </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,25 @@
+import { ThemeOptions } from '@mui/material/styles';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#41644a',
+    },
+    secondary: {
+      main: '#E86A33',
+    },
+    text: {
+      primary: '#111813',
+    },
+    error: {
+      main: '#d32f2f',
+    },
+    info: {
+      main: '#0288d1',
+    },
+    success: {
+      main: '#2e7d32',
+    },
+  },
+};
